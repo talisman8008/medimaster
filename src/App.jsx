6ce5b5da5f564d66f77e147ed1f2709918943d6a
Devesh <deveshhegde04@gmail.com>
2026-02-09 23:51:38 +0530

    comments fixxing

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ function App() {
     return (
         <Router>
             <Routes>
-                {/* Router ne ab LoginPage ko wrap kar liya, ab useNavigate chalega */}
+
                 <Route path="/" element={<LoginPage />} />
-                {/*<Route path="/receptionist" element={<ReceptionistDash />} />*/}
+
             </Routes>
         </Router>
     );
