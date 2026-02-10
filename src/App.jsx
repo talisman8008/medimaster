2ca3de0fd5bd68888c57ed588af21ebb7b75230f
talisman <deveshhegde04@gmail.com>
2026-02-10 09:43:45 +0530

    Change Router to HashRouter for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/Loginpage';
 
 function App() {
     return (
-        <Router>
+        <HashRouter>
             <Routes>
 
                 <Route path="/" element={<LoginPage />} />
 
             </Routes>
-        </Router>
+        </HashRouter>
     );
 }
 
